9138af77301f9e99c1d64e3bb39605be39ff6a10
Overlander <anuragnandal@hotmail.com>
2022-09-24 08:38:29 -0400

    Change associations so that posts belong to an author instead of a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: "User"
 
   validates :title, presence: true, length: { in: 5..100 }
   validates :body, presence: true, length: { in: 1..500 }

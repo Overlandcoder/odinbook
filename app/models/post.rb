59358e6ff674e1e55bedf85cecde0a59153d1a12
Overlander <anuragnandal@hotmail.com>
2022-09-24 09:01:01 -0400

    Add method to display post creation time nicely

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { in: 5..100 }
   validates :body, presence: true, length: { in: 1..500 }
+
+  def created_time_formatted
+    created_at.strftime("%b %-d, %Y - %l:%M %P")
+  end
 end

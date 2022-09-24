65be03edd6e550689783781f6e2a187e45f405d9
Overlander <anuragnandal@hotmail.com>
2022-09-23 20:24:59 -0400

    Add associations between User and Friendship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_193955) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_001608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_193955) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "friend_id"
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
@@ -41,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_193955) do
   end
 
   add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
 end

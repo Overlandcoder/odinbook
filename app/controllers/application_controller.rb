3daff7f91dffd95c67dceec1a9e8f21f0f01c36f
Overlander <anuragnandal@hotmail.com>
2022-09-25 10:28:37 -0400

    Overwrite sign_out redirect path method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+  
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end

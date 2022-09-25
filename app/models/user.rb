4576537a35d75ac87125392fe62ad05ac5d880e5
Overlander <anuragnandal@hotmail.com>
2022-09-24 22:52:52 -0400

    Fix association between users and friend requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :friendships
   has_many :friends, through: :friendships
-  has_many :friend_requests
+  has_many :friend_requests, foreign_key: :request_receiver_id
   has_many :request_receivers, through: :friend_requests
 
   validates :email, uniqueness: true

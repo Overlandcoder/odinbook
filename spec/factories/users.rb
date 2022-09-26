939c5d2690351988bba9250c0f6e7ab462b64d34
Overlander <anuragnandal@hotmail.com>
2022-09-25 22:28:53 -0400

    Use Faker's username method instead of name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Name.name }
+    username { Faker::Internet.username }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
     id { integer }

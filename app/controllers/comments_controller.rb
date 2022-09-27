142680b1089c556e7dda88488ae177f1d6e74752
Overlander <anuragnandal@hotmail.com>
2022-09-27 10:06:37 -0400

    Implement actions to allow comments to be created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+
+    if @comment.save!
+      redirect_to post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+      flash.now[:error] = "Comment couldn't be posted."
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :commenter_id)
+  end
 end

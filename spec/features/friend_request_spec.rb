a32e69b06131088d47a9072446ab49e29891279f
Overlander <anuragnandal@hotmail.com>
2022-09-29 08:10:05 -0400

    Move some tests from user_spec file to other test files

--- spec/features/friend_request_spec.rb
+++ spec/features/friend_request_spec.rb
@@ -24,6 +24,38 @@ RSpec.describe 'FriendRequest', type: :feature do
       expect { click_on "Add Friend" }.
         to change { FriendRequest.all.count }.from(0).to(1)
     end
+
+    it 'sends the request to the correct user' do
+      click_on "Add Friend"
+      visit user_path(user2.id)
+      expect(page).to have_button 'Cancel Friend Request'
+    end
+
+    it 'other user receives the request from the correct user' do
+      click_on "Add Friend"
+      login_as(user2)
+      visit user_path(user2.id)
+      expect(page).to have_content 'Friend Requests: 1'
+    end
+  end
+
+  context 'when a user has a friend request' do
+    before do
+      login_as(user1)
+      visit user_path(user2.id)
+      click_on "Add Friend"
+      login_as(user2)
+      visit user_path(user2.id)
+    end
+
+    it 'displays the correct amount of friend requests' do
+      expect(page).to have_content "Friend Requests: 1"
+    end
+
+    it 'shows the correct friend request' do
+      visit user_friend_requests_path(user2.id)
+      expect(page).to have_content "From: #{user1.username}"
+    end
   end
 
   context 'when a friend request is accepted' do
--- spec/features/friendship_spec.rb
+++ spec/features/friendship_spec.rb
@@ -3,42 +3,45 @@ require 'rails_helper'
 RSpec.describe 'Friendship', type: :feature do
   let!(:user1) { FactoryBot.create(:user, id: 1) }
   let!(:user2) { FactoryBot.create(:user, id: 2) }
-  let!(:user3) { FactoryBot.create(:user, id: 3) }
 
-  before do
-    login_as(user1)
-    visit user_path(user2.id)
-    click_on "Add Friend"
-    login_as(user2)
-    visit user_friend_requests_path(user2.id)
-    click_on "Accept Request"
-  end
-
-  context 'when viewing own friend list' do
+  context 'when a user accepts a friend request' do
     before do
-      visit user_friendships_path(user2.id)
+      login_as(user1)
+      visit user_path(user2.id)
+      click_on "Add Friend"
+      login_as(user2)
+      visit user_friend_requests_path(user2.id)
+      click_on "Accept Request"
     end
-
-    it 'lists the correct user as a friend' do
+  
+    it 'creates the friendship for the request receiver' do
+      visit user_friendships_path(user2.id)
       expect(page).to have_link "#{user1.username.capitalize}"
     end
 
-    it 'does not show self as a friend' do
+    it 'does not add self as friend of self' do
+      visit user_friendships_path(user2.id)
       expect(page).not_to have_link "#{user2.username.capitalize}"
     end
-  end
 
-  context 'when viewing friend list of friend' do
-    before do
+    it 'does not add self as friend of self' do
       visit user_friendships_path(user1.id)
+      expect(page).not_to have_link "#{user1.username.capitalize}"
     end
 
-    it 'lists the correct user as a friend' do
+    it 'creates the friendship for the request sender' do
+      visit user_friendships_path(user1.id)
       expect(page).to have_link "#{user2.username.capitalize}"
     end
 
-    it 'does not show self as a friend' do
-      expect(page).not_to have_link "#{user1.username.capitalize}"
+    it 'increments friends of request receiver by 1' do
+      visit user_path(user2.id)
+      expect(page).to have_content 'Friends: 1'
+    end
+
+    it 'increments friends of request sender by 1' do
+      visit user_path(user1.id)
+      expect(page).to have_content 'Friends: 1'
     end
   end
 end

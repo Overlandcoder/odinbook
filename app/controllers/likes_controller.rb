95f6eb5d140e3c921e8edc106c5cd276e18070ed
Overlander <anuragnandal@hotmail.com>
2022-09-30 15:21:54 -0400

    Remove redirections

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,15 +6,12 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.create(like_params)
-    redirect_to post_path(@post)
   end
 
   def destroy
     @post = Post.find(params[:id])
     @like = @post.likes.find_by(like_params)
     @like.destroy
-
-    redirect_to @post
   end
 
   private

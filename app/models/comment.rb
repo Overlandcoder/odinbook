dc7b4d0cafc92fe06b6a4fcd0147c44f26bbacdb
Overlander <anuragnandal@hotmail.com>
2022-10-06 12:55:13 -0400

    Redesign post layout (comments now shown in the posts)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, presence: true, length: { minimum: 1 }
+
+  def created_time_formatted
+    created_at.strftime("%b %-d, %Y - %l:%M %P")
+  end
 end

71b26e292ad7ab04648956742e6dab1c912d955c
Overlander <anuragnandal@hotmail.com>
2022-10-07 19:54:53 -0400

    Update comment tests

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :post do
     association :author
-    title { "MyString" }
     body { "MyString" }
   end
 end
--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -5,19 +5,14 @@ RSpec.describe 'Comment', type: :feature do
   let!(:user2) { FactoryBot.create(:user, id: 2) }
   let!(:post1) { FactoryBot.create(:post, author: user2) }
 
-  before do
-    login_as(user1)
-    visit root_path
-    click_on 'Create Post'
-    fill_in 'Title', with: 'first post'
-    fill_in 'Body', with: 'my text'
-    click_on 'Submit Post'
-  end
-
   context 'when adding a comment' do
     before do
-      fill_in 'New Comment', with: 'some comment text'
+      login_as(user1)
+      visit root_path
+      click_on 'Comment'
+      fill_in 'Comment:', with: 'some comment text'
       click_on 'Post Comment'
+      visit root_path
     end
 
     it 'adds the comment' do
@@ -29,18 +24,18 @@ RSpec.describe 'Comment', type: :feature do
     end
 
     it 'increments comment count of the post by 1' do
-      visit post_path(post1)
-      fill_in 'New Comment', with: 'a comment'
-      click_on 'Post Comment'
       expect(post1.comments.count).to eq(1)
     end
   end
 
   context 'when viewing a post with a comment' do
     before do
+      login_as(user1)
       visit post_path(post1)
-      fill_in 'New Comment', with: 'a comment'
+      click_on 'Comment'
+      fill_in 'Comment:', with: 'a comment'
       click_on 'Post Comment'
+      visit post_path(post1)
     end
 
     it 'displays the post' do

2a877de7e3e366adffc066177a122f002dc90983
Overlander <anuragnandal@hotmail.com>
2022-10-09 12:31:11 -0400

    Generate Profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_181819) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_09_163027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_181819) do
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "gender"
+    t.integer "age"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :profile do
+    name { "MyString" }
+    gender { "MyString" }
+    age { 1 }
+    location { "MyString" }
+  end
+end

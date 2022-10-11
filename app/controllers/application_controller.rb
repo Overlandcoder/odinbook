036b9b32cd530b1b5b1a9c3d131624c7b52e1102
Overlander <anuragnandal@hotmail.com>
2022-10-11 14:47:48 -0400

    Remove custom sign out method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
-  
-  private
-
-  # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
 end

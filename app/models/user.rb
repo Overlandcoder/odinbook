85a24d7719eec81ba91d15ea6c6336dd74a08719
Overlander <anuragnandal@hotmail.com>
2022-10-11 15:31:25 -0400

    Limit suggested users to 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,14 +50,16 @@ class User < ApplicationRecord
   end
 
   def random_strangers
-    20.times.map { User.all.sample }.uniq.reject { |user| !user.can_send_request_to?(self) }
+    50.times.map { User.all.sample }
+      .uniq.select { |user| can_send_request_to?(user) }
+      .take(5)
   end
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.username = auth.info.name   # assuming the user model has a name
+      user.username = auth.info.name # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.

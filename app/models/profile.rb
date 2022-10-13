26245ecebb9d25d5617701b8149f7e043e59d6ce
Overlander <anuragnandal@hotmail.com>
2022-10-13 12:55:50 -0400

    Improve layout of personal info on user profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,18 +2,18 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   def display_name
-    name.empty? ? "-" : name.titleize
+    name.empty? ? "" : name.titleize
   end
 
   def display_gender
-    gender.empty? ? "-" : gender.capitalize
+    gender.empty? ? "" : gender.capitalize
   end
 
   def display_age
-    age.nil? ? "-" : age
+    age.nil? ? "" : age
   end
 
   def display_location
-    location.empty? ? "-" : location.capitalize
+    location.empty? ? "" : location.capitalize
   end
 end
